022ba9d88f8e5a378d39f7e564c4ae1bfbd76364
adsick <adsick@protonmail.com>
2022-01-12 19:25:40 +0200

    refactoring

--- market/src/inner.rs
+++ market/src/inner.rs
@@ -1,18 +1,25 @@
-use crate::Market;
 use crate::common::*;
-use crate::sale::{Sale, Bid, DELIMETER};
+use crate::sale::{Bid, Sale, DELIMETER};
+use crate::Market;
 
-impl Market{
-
+impl Market {
     pub(crate) fn internal_remove_sale(
         &mut self,
         nft_contract_id: AccountId,
         token_id: TokenId,
     ) -> Sale {
         let contract_and_token_id = format!("{}{}{}", &nft_contract_id, DELIMETER, token_id);
-        let sale = self.market.sales.remove(&contract_and_token_id).expect("No sale");
+        let sale = self
+            .market
+            .sales
+            .remove(&contract_and_token_id)
+            .expect("No sale");
 
-        let mut by_owner_id = self.market.by_owner_id.get(&sale.owner_id).expect("No sale by_owner_id");
+        let mut by_owner_id = self
+            .market
+            .by_owner_id
+            .get(&sale.owner_id)
+            .expect("No sale by_owner_id");
         by_owner_id.remove(&contract_and_token_id);
         if by_owner_id.is_empty() {
             self.market.by_owner_id.remove(&sale.owner_id);
@@ -29,19 +36,28 @@ impl Market{
         if by_nft_contract_id.is_empty() {
             self.market.by_nft_contract_id.remove(&nft_contract_id);
         } else {
-            self.market.by_nft_contract_id
+            self.market
+                .by_nft_contract_id
                 .insert(&nft_contract_id, &by_nft_contract_id);
         }
 
         // here AccountId is used as "token type", idk why so (adsick)
         let token_type = sale.token_type.clone();
         if let Some(token_type) = token_type {
-            let mut by_nft_token_type = self.market.by_nft_token_type.get(&token_type.parse().unwrap()).expect("No sale by nft_token_type");
+            let mut by_nft_token_type = self
+                .market
+                .by_nft_token_type
+                .get(&token_type.parse().unwrap())
+                .expect("No sale by nft_token_type");
             by_nft_token_type.remove(&contract_and_token_id);
             if by_nft_token_type.is_empty() {
-                self.market.by_nft_token_type.remove(&token_type.parse().unwrap());
+                self.market
+                    .by_nft_token_type
+                    .remove(&token_type.parse().unwrap());
             } else {
-                self.market.by_nft_token_type.insert(&token_type.parse().unwrap(), &by_nft_token_type);
+                self.market
+                    .by_nft_token_type
+                    .insert(&token_type.parse().unwrap(), &by_nft_token_type);
             }
         }
 
@@ -60,24 +76,26 @@ impl Market{
             .sales
             .get(&contract_and_token_id)
             .expect("No sale");
-        let bid_vec = sale
-            .bids
-            .get(&token_id.parse().unwrap())
-            .expect("No token");
+        let bid_vec = sale.bids.get(&token_id.parse().unwrap()).expect("No token");
+
+        let mut sale = self
+            .market
+            .sales
+            .get(&contract_and_token_id) //тут мы по сути достаём значение из хранилища
+            .expect("No sale");
 
         for (index, bid_from_vec) in bid_vec.iter().enumerate() {
             if bid_from_vec.owner_id == bid.owner_id && bid_from_vec.price == bid.price {
-                self.market
-                    .sales
-                    .get(&contract_and_token_id)
-                    .expect("No sale")
-                    .bids
+                sale.bids
                     .get_mut(&token_id.parse().unwrap())
                     .expect("No token")
-                    .remove(index);
+                    .remove(index); // что-то с ним делаем
+
+                // но его ещё надо записать обратно
+                self.market.sales.insert(&contract_and_token_id, &sale);
             };
         }
 
-        self.market.sales.get(&contract_and_token_id).unwrap()
+        sale
     }
 }
