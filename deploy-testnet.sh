1de89907229651f4b6f277362a609d40131ee37e
Buckram123 <m.donchenko@4irelabs.com>
2022-01-18 11:06:08 +0200

    fixing .env file

--- deploy-testnet.sh
+++ deploy-testnet.sh
@@ -9,12 +9,16 @@ rm -rvf neardev
 
 near dev-deploy res/nft_bid_market.wasm
 source neardev/dev-account.env
-cat neardev/dev-account.env > .env
 
 CONTRACT_PARENT=$CONTRACT_NAME
 MARKET_CONTRACT_ID=$CONTRACT_PARENT
 NFT_CONTRACT_ID=nft.$CONTRACT_PARENT
 ALICE=alice.$CONTRACT_PARENT
+
+echo "CONTRACT_PARENT=$CONTRACT_NAME" > .env
+echo "MARKET_CONTRACT_ID=$CONTRACT_PARENT" >> .env
+echo "NFT_CONTRACT_ID=nft.$CONTRACT_PARENT" >> .env
+echo "ALICE=alice.$CONTRACT_PARENT" >> .env
 
 set -e
 near create-account $NFT_CONTRACT_ID --masterAccount $CONTRACT_PARENT --initialBalance "27"
